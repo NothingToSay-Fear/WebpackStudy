40c21a12f424619572c1782a029d57b8a8fdb82f
Fear <1143533573@qq.com>
2025-01-06 22:05:52 +0800

    升级为react18

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,7 @@
-/*
- * @Description: 
- * @Date: 2022-05-25 14:34:57
- * @LastEditTime: 2022-07-14 14:37:12
- * @Author: Bin
- */
-// import "react-app-polyfill/ie11";
-// import 'core-js/stable';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-  <App></App>
-  ,
-  document.getElementById('root')
-)
+root.render(<App />)
